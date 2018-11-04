b527ff7ed25f4ae310133ea8dbb7d7d8545057e2
foxniver <32783730+foxniver@users.noreply.github.com>
2018-11-03 22:02:10 -0500

    Updating service stubs.

--- src/main/java/com/settleUpService/SettleUpService.java
+++ src/main/java/com/settleUpService/SettleUpService.java
@@ -1,19 +1,7 @@
 package com.settleUpService;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-import java.util.HashSet;
-import java.util.Set;
 
-@ApplicationPath("/services")
-public class SettleUpService extends Application {
 
-    @Override
-    public Set<Class<?>> getClasses() {
+public class SettleUpService {
 
-        Set set = new HashSet<Class<?>>();
-        set.add(SettleUpService.class);
-
-        return set;
-    }
 }
--- src/main/java/com/settleUpService/SettleUpServiceConfiguration.java
+++ src/main/java/com/settleUpService/SettleUpServiceConfiguration.java
@@ -1,4 +1,19 @@
 package com.settleUpService;
 
-public class SettleUpServiceConfiguration {
+import javax.ws.rs.ApplicationPath;
+import java.util.HashSet;
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/services")
+public class SettleUpServiceConfiguration extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+
+        Set set = new HashSet<Class<?>>();
+        set.add(SettleUpService.class);
+
+        return set;
+    }
 }
