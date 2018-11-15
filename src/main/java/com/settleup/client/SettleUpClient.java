bb25fb5e41e948be18f58ff5a710ba7e7fb94aa2
kdkoberle <kdkoberle@madisoncollege.edu>
2018-11-14 18:45:51 -0600

    json update

--- src/main/java/com/settleup/client/SettleUpClient.java
+++ src/main/java/com/settleup/client/SettleUpClient.java
@@ -2,6 +2,7 @@ package com.settleup.client;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.settleup.entity.SettleUp;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -11,7 +12,7 @@ import java.util.List;
 
 public class SettleUpClient {
 
-    public List<JSONResult> getJSONRestuls(int rent, String activity, int numberBedrooms) throws Exception {
+    public List<SettleUp> getJSONRestuls(int rent, String activity, int numberBedrooms) throws Exception {
         Client client = ClientBuilder.newClient();
         String targetUrl = "http://18.216.201.147:8080/settleup/services/settleUpService/json/" + rent + "/" + activity + "/" + numberBedrooms;
         WebTarget target = client.target(targetUrl);
@@ -19,14 +20,26 @@ public class SettleUpClient {
         String respone = target.request(MediaType.APPLICATION_JSON).get(String.class);
 
         ObjectMapper mapper = new ObjectMapper();
-        List<JSONResult> results = mapper.readValue(respone, new TypeReference<List<JSONResult>>() {
+        List<SettleUp> results = mapper.readValue(respone, new TypeReference<List<SettleUp>>() {
         });
 
         return results;
     }
 
-    //public List<XMLResult> getXMLResults(int rent, String activity, int numberBedrooms) throws Exception {
+    public List<SettleUp> getXMLResults(int rent, String activity, int numberBedrooms) throws Exception {
+        Client client = ClientBuilder.newClient();
+        String targetUrl = "http://18.216.201.147:8080/settleup/services/settleUpService/json/" + rent + "/" + activity + "/" + numberBedrooms;
+        WebTarget target = client.target(targetUrl);
 
-    //}
+        String respone = target.request(MediaType.APPLICATION_JSON).get(String.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        List<SettleUp> results = mapper.readValue(respone, new TypeReference<List<SettleUp>>() {
+        });
+
+        return results;
+
+
+    }
 
 }
--- src/test/java/com/settleup/client/SettleUpClientTest.java
+++ src/test/java/com/settleup/client/SettleUpClientTest.java
@@ -13,6 +13,7 @@ import javax.ws.rs.core.Response;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.UnmarshalException;
 import javax.xml.bind.Unmarshaller;
+import java.io.StringReader;
 import java.util.List;
 import java.util.Set;
 
@@ -25,27 +26,29 @@ class SettleUpClientTest {
         Client client = ClientBuilder.newClient();
         WebTarget target = client.target("http://18.216.201.147:8080/settleup/services/settleUpService/json/800/Sports/2");
 
-        String respone = target.request(MediaType.APPLICATION_JSON).get(String.class);
+        String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
 
         ObjectMapper mapper = new ObjectMapper();
-        List<JSONResult> results = mapper.readValue(respone, new TypeReference<List<JSONResult>>(){});
+        List<SettleUp> results = mapper.readValue(response, new TypeReference<List<SettleUp>>(){});
 
-        assertEquals(630, results.get(0).getRent1());
+        assertEquals(630, results.get(0).getRent_1());
     }
 
 
-   /** @Test
+    @Test
     public void testXML() throws Exception {
         Client client = ClientBuilder.newClient();
         WebTarget target = client.target("http://18.216.201.147:8080/settleup/services/settleUpService/xml/800/Sports/2");
 
-        String respone = target.request(MediaType.APPLICATION_XML).get(String.class);
+        String response = target.request(MediaType.APPLICATION_XML).get(String.class);
+
         JAXBContext jaxbContext = JAXBContext.newInstance(SettleUp.class);
         Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-        SettleUp results = (SettleUp) unmarshaller.unmarshal("http://18.216.201.147:8080/settleup/services/settleUpService/xml/800/Sports/2");
 
-        assertEquals("???", respone);
-    }*/
+        SettleUp results = (SettleUp)unmarshaller.unmarshal(new StringReader(response));
+
+        assertEquals("???", response);
+    }
 
 
 }
