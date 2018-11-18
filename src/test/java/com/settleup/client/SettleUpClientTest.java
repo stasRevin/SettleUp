7d40aaae4cd0a99b080c851d78f2cd335a9681a6
Sasha <oleksanova@gmail.com>
2018-11-18 15:00:35 -0600

    exception tests.

--- src/test/java/com/settleup/client/SettleUpClientTest.java
+++ src/test/java/com/settleup/client/SettleUpClientTest.java
@@ -3,8 +3,12 @@ package com.settleup.client;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import javassist.NotFoundException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import com.settleup.entity.SettleUp;
+import org.mockito.internal.matchers.Not;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -12,13 +16,23 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamReader;
+import java.io.IOException;
 import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.List;
 
+import static javax.print.attribute.standard.ReferenceUriSchemesSupported.HTTP;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class SettleUpClientTest {
-/*
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+
+    /**
+     * Method to test service to return json object with the rent price
+     * @throws Exception
+     */
     @Test
     public void testJson() throws Exception {
         Client client = ClientBuilder.newClient();
@@ -33,6 +47,10 @@ class SettleUpClientTest {
     }
 
 
+    /**
+     * Method to test service to return xml object with the rent price
+     * @throws Exception
+     */
     @Test
     public void testXML() throws Exception {
         Client client = ClientBuilder.newClient();
@@ -52,5 +70,46 @@ class SettleUpClientTest {
 
         assertEquals(630, results.get(0).getRent_1());
     }
-*/
+
+
+    /**
+     * Method to test if 404 exception been thrown by the service
+     * if user enters rent price that does not exist.
+     * Test json type request
+     */
+    @Test
+    public void getResponseExceptionJsonSuccess() {
+
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://18.216.201.147:8080/settleup/services/settleUpService/json/8000/Sports/2");
+
+        try {
+            assertThrows(javax.ws.rs.NotFoundException.class, () ->  target.request(MediaType.APPLICATION_JSON).get(String.class));
+
+        } catch (Exception exception) {
+            logger.error(exception);
+        }
+
+    }
+
+
+    /**
+     * Method to test if 404 exception been thrown by the service
+     * if user enters rent price that does not exist.
+     * Test xml type request
+     */
+    @Test
+    public void getResponseExceptionXMLSuccess() {
+
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://18.216.201.147:8080/settleup/services/settleUpService/xml/8000/Sports/2");
+
+        try {
+            assertThrows(javax.ws.rs.NotFoundException.class, () ->  target.request(MediaType.APPLICATION_XML).get(String.class));
+
+        } catch (Exception exception) {
+            logger.error(exception);
+        }
+
+    }
 }
